e015c60e6d73226dcf0e1d76b8e6b4bd5dfea1fc
SitNis <rus.lol2011@yandex.ru>
2023-06-04 21:54:00 +0300

    refactoring

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,4 +1,10 @@
+# rubocop:disable all
+
 class GildedRose
+  AGED_BRIE = 'Aged Brie'
+  SULFURAS = 'Sulfuras, Hand of Ragnaros'
+  BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert'
+  CONJURED_ITEM_REGEXP = /^Conjured/
 
   def initialize(items)
     @items = items
@@ -6,50 +12,85 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
+      before_day_gone_updates(item)
+
+      day_passed(item)
+
       if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+        after_day_gone_updates(item)
       end
     end
+  end
+
+  private
+
+  def before_day_gone_updates(item)
+    case item
+    when AGED_BRIE
+      update_brie(item)
+    when BACKSTAGE_PASSES
+      update_backstage_pass(item)
+    when CONJURED_ITEM_REGEXP
+      update_conjured(item)
+    else
+      update_item(item)
+    end
+  end
+
+  def after_day_gone_updates(item)
+    case item
+    when AGED_BRIE
+      increase_quality(item)
+    when BACKSTAGE_PASSES
+      concert_passed(item)
+    when CONJURED_ITEM_REGEXP
+      update_conjured(item)
+    else
+      update_item(item)
+    end
+  end
+
+  def decrease_quality(item)
+    item.quality -= 1 if item.quality > 0
+  end
+
+  def increase_quality(item)
+    item.quality += 1 if item.quality < 50
+  end
+
+  def update_conjured(item)
+    decrease_quality(item)
+    decrease_quality(item)
+  end
+
+  def update_item(item)
+    decrease_quality(item)
+  end
+
+  def update_backstage_pass(item)
+    increase_quality(item)
+
+    if item.sell_in < 11
+      increase_quality(item)
+    end
+
+    if item.sell_in < 6
+      increase_quality(item)
+    end
+  end
+
+  def update_brie(item)
+    increase_quality(item)
+  end
+
+  def day_passed(item)
+    return if item.name == SULFURAS
+
+    item.sell_in -= 1
+  end
+
+  def concert_passed(item)
+    item.quality = 0
   end
 end
 
@@ -62,7 +103,7 @@ class Item
     @quality = quality
   end
 
-  def to_s()
+  def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
